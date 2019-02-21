a0cdd2331790c80107b020b1d562d075a4aa0838
Penny <pennyafnorberg@hotmail.com>
2019-02-21 13:44:53 +0100

    Added auth db

---
+++ Fullstacklabb/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Fullstacklabb.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Fullstacklabb.Areas.Identity.IdentityHostingStartup))]
+namespace Fullstacklabb.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<FullstacklabbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("FullstacklabbContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<FullstacklabbContext>();
+            });
+        }
+    }
+}
