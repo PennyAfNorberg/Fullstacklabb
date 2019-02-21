2be68d385a49926878229daba2d66a1a8acc7b47
Penny <pennyafnorberg@hotmail.com>
2019-02-21 14:31:50 +0100

    Added seed for genres

---
+++ Fullstacklabb/Migrations/20190221131814_popgenretabel.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fullstacklabb.Migrations
+{
+    public partial class popgenretabel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+               table: "Genres",
+               columns: new[] { "Id", "Name" },
+               values: new object[,]
+               {
+                            { "1", "Jazz"},
+                            { "2", "Pop" },
+                            { "3", "Rock" },
+                            { "4", "Rap" },
+                            { "5", "House" },
+               });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.DeleteData(
+                 table: "Genres",
+                 keyColumn: "Id",
+                 keyValue: "1");
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: "2");
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: "3");
+            migrationBuilder.DeleteData(
+               table: "Genres",
+               keyColumn: "Id",
+               keyValue: "4");
+            migrationBuilder.DeleteData(
+               table: "Genres",
+               keyColumn: "Id",
+               keyValue: "5");
+
+        }
+    }
+}
